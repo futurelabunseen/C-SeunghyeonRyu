f37aabaf20957e3d721f414494a22660ba63610e
Seunghyeon Ryu <play2ski@gmail.com>
2024-04-12 11:05:24 +0900

    PreAttributeChange에서 CurrentValue만 수정하는데, GE에서 BaseValue를 사용해서 생기는 문제 발생. 임시로 AttributeChange 시 BaseValue도 수정하는 것으로 해결.

--- Unseen/Source/Unseen/AbilitySystem/Attribute/UnseenCharacterAttributeSet.cpp
+++ Unseen/Source/Unseen/AbilitySystem/Attribute/UnseenCharacterAttributeSet.cpp
@@ -17,17 +17,17 @@ void UUnseenCharacterAttributeSet::PreAttributeChange(const FGameplayAttribute& 
 {
 	if (Attribute == GetStaminaAttribute())
 	{
-		UE_LOG(LogTemp, Warning, TEXT("%f"), NewValue);
+		//UE_LOG(LogTemp, Warning, TEXT("Before Clamp : %f"), NewValue);
 		
 		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxStamina());
 		
-		UE_LOG(LogTemp, Warning, TEXT("%f"), NewValue);
+		//UE_LOG(LogTemp, Warning, TEXT("After Clamp : %f"), NewValue);
 
 		// 에디터 디버깅 창에는 100으로 나온다.
 		// 그러나 어째서?
 		// Base Value와 Current Value가 나뉘어있다.
 		// 이거 관련 문제인듯
-		// Attribute set에서는 GetCurrentValue만 바꾸는듯.
+		// Attribute set에서는 CurrentValue만 바꾸는듯.
 	}
 	Super::PreAttributeChange(Attribute, NewValue);
 }
@@ -35,4 +35,11 @@ void UUnseenCharacterAttributeSet::PreAttributeChange(const FGameplayAttribute& 
 void UUnseenCharacterAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
 {
 	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+	if (Attribute == GetStaminaAttribute())
+	{
+		GetStaminaAttribute().GetGameplayAttributeData(this)->SetBaseValue(NewValue);
+		//GetStaminaAttribute().GetGameplayAttributeData(this)->SetCurrentValue(NewValue);
+		//UE_LOG(LogTemp, Warning, TEXT("Get Base Value : %f"), GetStaminaAttribute().GetGameplayAttributeData(this)->GetBaseValue());
+		//UE_LOG(LogTemp, Warning, TEXT("Get Current Value : %f"), GetStaminaAttribute().GetGameplayAttributeData(this)->GetCurrentValue());
+	}
 }
